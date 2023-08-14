d1a1588f542e9a27dfeebeaffb423d1bea3651cf
EduardoCastA <a311946@alumnos.uaslp.mx>
2023-08-14 07:37:42 -0600

    Agregar menú

--- lib/home.dart
+++ lib/home.dart
@@ -21,13 +21,17 @@ import 'model/products_repository.dart';
 import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
 
-  // TODO: Add a variable for Category (104)
+  // TODO: Add a variable for Category (104) --
+  final Category category;
+
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104) --
-    return AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
-    // TODO: Pass Category variable to AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104) --
+    return AsymmetricView(
+        products: ProductsRepository.loadProducts(category)
+    );
   }
 }

78196aa8f679f4d9ae58283c02fdcddd2be58797
EduardoCastA <a311946@alumnos.uaslp.mx>
2023-08-13 20:47:37 -0600

    Agregar shape

--- lib/app.dart
+++ lib/app.dart
@@ -69,6 +69,10 @@ ThemeData _buildShrineTheme() {
     textSelectionTheme: const TextSelectionThemeData(
       selectionColor: kShrinePink100,
     ),
+    appBarTheme: const AppBarTheme(
+      foregroundColor: kShrineBrown900,
+      backgroundColor: kShrinePink100,
+    ),
     // TODO: Decorate the inputs (103) --
     inputDecorationTheme: const InputDecorationTheme(
       border: CutCornersBorder(),
